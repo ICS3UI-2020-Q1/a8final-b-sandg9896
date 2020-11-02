3e542def410e0d2a4fb92cbbff14d7d2724ded09
sandg9896 <sandg9896@wrdsb.ca>
2020-11-02 14:34:17 +0000

    A8 Final

--- Main.java
+++ Main.java
@@ -5,8 +5,14 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
 
+  JLabel countLabel;
+
+  JButton resetButton;
+  JButton countButton;
+  
+  JTextArea outputArea;
 
   // Method to assemble our GUI
   public void run(){
@@ -18,8 +24,46 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+    // initialize the main JPanel
+    mainPanel = new JPanel();
+    // turn on the manual layouts
+    mainPanel.setLayout(null);
+    // add the panel to the JFrame to see it
+    frame.add(mainPanel);
  
-    
+   // create the Label to let the user know what to enter
+   countLabel = new JLabel("Counter:");
+   // set location of count label
+   countLabel.setBounds(100, 70, 150, 20);
+   // add label to output area
+   mainPanel.add(countLabel);
+
+   
+
+   // initialize the JButtons
+   countButton = new JButton("count");
+   resetButton = new JButton("reset");
+   // set the location and size of the buttons
+   countButton.setBounds(80, 10, 100, 35);
+   resetButton.setBounds(220, 10, 100, 35);
+   // add the actionListener to the buttons
+   countButton.addActionListener(this);
+   resetButton.addActionListener(this);
+   // give each button an actionCommand
+   countButton.setActionCommand("count");
+   resetButton.setActionCommand("reset");
+   // add the buttons to the main panel
+   mainPanel.add(countButton);
+   mainPanel.add(resetButton);
+
+   // initialize the JButtons
+   outputArea = new JTextArea("0");
+   // set the location and size of the buttons
+   outputArea.setBounds(200, 70, 100, 20);
+   // disable the textAreas so that the user can't type in them
+   outputArea.setEnabled(false);
+   // add the button to the main panel
+   mainPanel.add(outputArea);
 
   }
 
@@ -28,8 +72,25 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
-  }
+    if(command.equals("count")){
+    // get text from each text boxes
+    String firstText = outputArea.getText();
+    //change the string into an integer
+    int convertedCount = Integer.parseInt(firstText);
+    // increase number by one if under 10
+    if(convertedCount < 10){
+    int sumConvertedCount = convertedCount + 1;
+    outputArea.setText("" + sumConvertedCount);
+     }
+    }
 
+    if(command.equals("reset")){
+     // clear the text in the box
+     outputArea.setText("0");
+    }
+   }
+  
+  
   // Main method to start our program
   public static void main(String[] args){
     // Creates an instance of our program
@@ -38,3 +99,4 @@ public class Main implements Runnable, ActionListener{
     SwingUtilities.invokeLater(gui);
   }
 }
+
